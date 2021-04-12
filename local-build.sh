b41de503e2c865d9800772177274ca4cd28be26d
Leenings, Marcel <marcel.leenings@adesso.de>
2021-04-12 14:04:21 +0200

    upd: local-build.sh

--- local-build.sh
+++ local-build.sh
@@ -2,3 +2,10 @@
 ionic build
 ionic capacitor build android
 ionic capacitor copy android && cd android && ./gradlew assembleDebug && cd ..
+
+#copy apk file to /bin
+from="./android/app/build/outputs/apk/debug/app-debug.apk"
+toFolder="bin"
+toFile="connect4.apk"
+to="$toFolder/$toFile"
+mkdir -p $toFolder && cp $from $to
