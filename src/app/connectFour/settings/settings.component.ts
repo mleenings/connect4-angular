4e1847517daef576652a49054aa250994f96c507
Leenings, Marcel <marcel.leenings@adesso.de>
2021-04-12 10:07:20 +0200

    fix: settings validation

--- src/app/connectFour/settings/settings.component.ts
+++ src/app/connectFour/settings/settings.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Output, Input, EventEmitter } from '@angular/core';
 import { SettingsModel } from './settings.model';
 import { CookieService } from 'ngx-cookie-service';
 import { properties } from '../properties/properties';
@@ -9,7 +9,8 @@ import { properties } from '../properties/properties';
   styleUrls: ['settings.component.scss']
 })
 export class SettingsComponent {
-  private settingsModel: SettingsModel;
+  @Input() settingsModel: SettingsModel;
+  @Output() settingsModelChanged = new EventEmitter<SettingsModel>();
 
   constructor(private cookieService: CookieService){
     this.loadSettings();
@@ -34,5 +35,23 @@ export class SettingsComponent {
 
   onChange(){
     this.cookieService.set(properties.cookie.names.settings, JSON.stringify(this.settingsModel));
+  }
+
+  onChangeRow(value){
+    this.settingsModel.numRow = this.settingsModel.numRow > 20 ? 20 : this.settingsModel.numRow;
+    this.settingsModel.numRow = this.settingsModel.numRow < 4 ? 4 : this.settingsModel.numRow;
+    this.onChange();
+    this.settingsModelChanged.emit(this.settingsModel);
+  }
+
+  onChangeColumn(value){
+    this.settingsModel.numColumn = this.settingsModel.numColumn > 20 ? 20 : this.settingsModel.numColumn;
+    this.settingsModel.numColumn = this.settingsModel.numColumn < 4 ? 4 : this.settingsModel.numColumn;
+    this.onChange();
+    this.settingsModelChanged.emit(this.settingsModel);
+  }
+
+  onFocusOut(){
+    window.location.reload();
   }
 }
